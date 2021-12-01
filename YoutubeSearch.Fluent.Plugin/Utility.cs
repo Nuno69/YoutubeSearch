563eeef47e5a831a16e35137bc4733b2fafa32f0
Nuno69 <nuno69a@gmail.com>
2021-12-01 17:39:15 +0100

    Basically implemented the plugin

--- YoutubeSearch.Fluent.Plugin/Utility.cs
+++ YoutubeSearch.Fluent.Plugin/Utility.cs
@@ -1,26 +1,26 @@
-﻿using Blast.API.Search;
-using YoutubeExplode;
-using YoutubeExplode.Common;
-using System.Collections.Generic;
+﻿//using Blast.API.Search;
+//using YoutubeExplode;
+//using YoutubeExplode.Common;
+//using System.Collections.Generic;
 
-namespace YoutubeSearch.Fluent.Plugin
-{
-	/// <summary>
-	/// Provides some common logic for querying Youtube data.
-	/// </summary>
-	internal class Utility
-	{
-		private YoutubeClient client;
-		internal async IAsyncEnumerable<YTSearchResult> VideoQuery(string q)
-		{
-			await foreach (var video in client.Search.GetVideosAsync(q))
-			{
-				yield return new(video, q);
-			}
-		}
-		public Utility()
-		{
-			client = new();
-		}
-	}
-	}
+//namespace YoutubeSearch.Fluent.Plugin
+//{
+//	/// <summary>
+//	/// Provides some common logic for querying Youtube data.
+//	/// </summary>
+//	internal class Utility
+//	{
+//		private YoutubeClient client;
+//		internal async IEnumerable<YTSearchResult> VideoQuery(string q)
+//		{
+//			await foreach (var video in client.Search.GetVideosAsync(q))
+//			{
+//				yield return new(video, q);
+//			}
+//		}
+//		public Utility()
+//		{
+//			client = new();
+//		}
+//	}
+//	}
--- YoutubeSearch.Fluent.Plugin/YTSearchApp.cs
+++ YoutubeSearch.Fluent.Plugin/YTSearchApp.cs
@@ -1,23 +1,68 @@
-﻿//using Blast.API.Search;
-//using Blast.Core;
-//using Blast.Core.Interfaces;
-//using Blast.Core.Objects;
-//using Blast.Core.Results;
-//using YoutubeExplode;
-//using YoutubeExplode.Common;
-//namespace YoutubeSearch.Fluent.Plugin
-//{
-//	public class YTSearchApp : ISearchApplication
-//	{
-//		private SearchApplicationInfo searchApplicationInfo;
-//		private readonly YoutubeClient client;
-//		public YTSearchApp()
-//		{
-//			client = new();
-//			searchApplicationInfo = new("Youtube search", "Allows to search Youtube without the annoying web browser", YTSearchResult.SupportedOperations);
-//			{
-//				SearchTagName = "Youtube";
-//			}
-//		}
-//	}
-//}
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Blast.API.Search;
+using Blast.Core;
+using Blast.Core.Interfaces;
+using Blast.Core.Objects;
+using Blast.Core.Results;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+
+namespace YoutubeSearch.Fluent.Plugin
+{
+	public class YTSearchApp : ISearchApplication
+	{
+		private readonly SearchApplicationInfo searchApplicationInfo;
+		private YoutubeClient client;
+		public YTSearchApp()
+		{
+			client = new();
+			searchApplicationInfo = new("YoutubeSearch", "Allows to search Youtube without leaving Fluent Search", YTSearchResult.SupportedOperations)
+			{
+				SearchTagName = "Youtube",
+				MinimumSearchLength = 3,
+				SearchTagOnly = true,
+				IsProcessSearchEnabled = false,
+				IsProcessSearchOffline = false,
+				ApplicationIconGlyph = null,
+				SearchAllTime = ApplicationSearchTime.Moderate,
+				DefaultSearchTags = new List<SearchTag>
+				{
+					new()
+					{
+						Name = "Youtube",
+						IconGlyph = null
+					}
+				}
+			};
+		} public SearchApplicationInfo GetApplicationInfo()
+		{
+			return searchApplicationInfo;
+		}
+		public async IAsyncEnumerable<ISearchResult> SearchAsync(SearchRequest searchRequest, CancellationToken cancellationToken)
+		{
+			string text = searchRequest.SearchedText;
+			string tag = searchRequest.SearchedTag;
+			if (String.IsNullOrWhiteSpace(text) || String.IsNullOrWhiteSpace(tag) || !tag.Equals("Youtube", StringComparison.OrdinalIgnoreCase))
+			{
+				yield break;
+			}
+			else
+			{
+				await foreach (var video in client.Search.GetVideosAsync(text))
+				{
+					yield return new YTSearchResult(video, text);
+				}
+			}
+		}
+		public ValueTask<IHandleResult> HandleSearchResult(ISearchResult searchResult)
+		{
+			if (searchResult is not YTSearchResult yTSearchResult)
+			{
+				return new ValueTask<IHandleResult>(new HandleResult(true, true));
+			}
+			var video = yTSearchResult.Video;
+			return new ValueTask<IHandleResult>(new HandleResult(true, true));
+		}
+	}
+}
