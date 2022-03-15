4cd4aa43f51a652839e00f0cb6f1af0dccc9bc3d
Nuno69 <nuno69a@gmail.com>
2022-03-15 22:21:01 +0100

    - Supposedly added the image preview.

--- YoutubeSearch.Fluent.Plugin/YTSearchResult.cs
+++ YoutubeSearch.Fluent.Plugin/YTSearchResult.cs
@@ -3,6 +3,7 @@ using Blast.Core.Interfaces;
 using Blast.Core.Objects;
 using Blast.Core.Results;
 using System.Collections.ObjectModel;
+using System.Net;
 using YoutubeExplode;
 using YoutubeExplode.Videos;
 namespace YoutubeSearch.Fluent.Plugin
@@ -14,11 +15,14 @@ namespace YoutubeSearch.Fluent.Plugin
 	{
 		internal static readonly ObservableCollection<ISearchOperation> SupportedOperations = new() { new YTOpenInBrowserOperation(), new YTDownloadVideoOperation(), new YTCopyURLOperation(), new YTPlayInExternalMediaPlayerOperation() };
 		private static readonly SearchTag[] SearchTags = { new() { Name = "Youtube" } };
+		private WebClient _client;
 		public IVideo Video { get; set; }
 		public YTSearchResult(IVideo video, string searchedText, ProcessInfo processInfo = null) : base(video.Title, searchedText, "Youtube",video.Title.SearchTokens(searchedText), SupportedOperations, SearchTags, processInfo)
 		{
+			_client = new WebClient();
 			Video = video;
 			AdditionalInformation = video.Author.Title;
+			PreviewImage = new BitmapImageResult( new MemoryStream(_client.DownloadData(video.Thumbnails.FirstOrDefault().Url)));
 			ShouldCacheResult = true;
 		}
 
